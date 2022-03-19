fe19ca18d18085de9a033cfc25c5a6fb4105d820
hosseinjvn <hoseinjavanbakht96@gmail.com>
2022-03-19 21:56:11 +0330

    016

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	card := newCard()
-	fmt.Println(card)
+	cards := []string{"Ace of shit", newCard()}
+	cards = append(cards, "six of mowzes")
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
