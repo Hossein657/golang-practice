0bcaa70e28a7bffda8198d1113c1e155418badd0
hosseinjvn <hoseinjavanbakht96@gmail.com>
2022-04-09 17:37:36 +0430

    043

--- main.go
+++ main.go
@@ -1,13 +1,37 @@
 package main
 
+import "fmt"
+
+type contactInfo struct {
+	email string
+	phone int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
 	alex := person{
 		firstName: "John",
 		lastName:  "doe",
+		contactInfo: contactInfo{
+			email: "john@example.com",
+			phone: 9099081202,
+		},
 	}
+	alexPointer := &alex
+	alexPointer.updateName("mamad")
+	alex.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+
 }
